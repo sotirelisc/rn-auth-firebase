939197446a35d1aefcadfdf3569cdd464cf0a21c
sotirelisc <gtakriz@hotmail.com>
2019-03-17 13:04:08 +0200

    Create login form with reusable Input component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -16,7 +17,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>Boom</Text>
+        <LoginForm />
       </View>
     );
   }

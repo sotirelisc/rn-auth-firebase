2259d481f70ce72775e4a79762098a7ca6572a99
sotirelisc <gtakriz@hotmail.com>
2019-03-17 20:44:53 +0200

    Show login form based on auth state.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  // Use null because we need 3 states:
+  // 1. Unknown if logged-in (null)
+  // 2. Definitely logged-in (true)
+  // 3. Definitely not logged-in (false)
+  state = { loggedIn: null };
+
   componentWillMount() {
     const config = {};
 
     firebase.initializeApp(config);
 
     console.log(config);
+
+    // Tracks authentication state
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button onPress={}>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
